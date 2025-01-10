7a4c758faae4c85400253e863e6e68e336a4b167
Isaac Tewolde <isaac@tewolde.co>
2025-01-10 14:18:29 +0100

    musicKit library added

--- src/module.ts
+++ src/module.ts
@@ -16,10 +16,18 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  async setup(_options, _nuxt) {
+  async setup(_options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     await installModule('@nuxt/scripts');
+
+    nuxt.options.app.head.script ||= []
+    // Extend Nuxt config to load the MusicKit.js library
+    nuxt.options.app.head.script.push({
+      src: 'https://js-cdn.music.apple.com/musickit/v3/musickit.js',
+      defer: true,
+      'data-web-components': true,
+    });
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
