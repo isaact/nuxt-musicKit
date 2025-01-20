1a1daaf72bdec7cc4081a22d73befa651d02d864
Isaac Tewolde <isaac@tewolde.co>
2025-01-20 20:57:18 +0100

    wip

--- src/runtime/server/utils/musicKit.ts
+++ src/runtime/server/utils/musicKit.ts
@@ -23,7 +23,7 @@ export async function generateDeveloperToken(privateKey: string, teamId: string,
 
         return token
     } catch (error) {
-        console.error(`Failed to generate developer token (keylen: ${privateKey.length}, ${teamId}, ${keyId}):`, error)
+        console.error(`Failed to generate developer token (keylen: ${privateKey.length}, ${teamId.length}, ${keyId.length}):`, error)
         throw error
     }
 }
